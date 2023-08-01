3f03b639c0dd8174b24f0e0c3d2d501f06d32328
Andyfanfan <985897924@qq.com>
2023-08-01 10:34:24 -0700

    optimize

--- app/src/main/java/com/andyfan/hackathon/Store.kt
+++ app/src/main/java/com/andyfan/hackathon/Store.kt
@@ -3,7 +3,7 @@ package com.andyfan.hackathon
 object Store {
     var message: String = ""
     var photoId: Int = 0
-    var videoUrl: String = "https://andyfanfan.myds.me/kodexplorer/data/User/admin/home/%E8%A7%86%E9%A2%91/SampleVideo_1280x720_1mb.mp4"
+    var videoUrl: String = "https://andyfanfan.myds.me/kodexplorer/data/User/admin/home/%E8%A7%86%E9%A2%91/my%20speaking%20mona%20lisa.mp4"
 }
 
 object Utils {
